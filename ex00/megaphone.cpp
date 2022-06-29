dc083b9f9c5e75c2b4b20b9e11ae0fc266db13ae
Anderson Porto <anderson.higo2@gmail.com>
2022-06-28 22:41:05 -0300

    build: megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 1) {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j] != '\0'; j++) {
+				std::cout << (char)toupper(argv[i][j]);
+			}
+			if (i != argc - 1)
+				std::cout << " ";
+		}
+		return 0;
+	}
+	else {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+}
