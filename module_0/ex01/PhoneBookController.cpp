934c33170bcfb7cef3c047a81325d779e29ed845
Anderson Porto <anderson.higo2@gmail.com>
2022-08-09 00:46:17 -0300

    add: module0

---
+++ module_0/ex01/PhoneBookController.cpp
@@ -0,0 +1,25 @@
+#include "PhoneBookController.hpp"
+
+PhoneController::PhoneController()
+{
+}
+
+PhoneController::~PhoneController()
+{
+}
+
+void	PhoneController::phoneBookService()
+{
+	std::string commandString;
+	std::cout << "This is the awesome phonebook !!" <<  std::endl;
+	for(;;) {
+		std::cout << "you can use one of the following commands: " << CMD << std::endl;
+		std::getline(std::cin, commandString);
+		if (commandString == "ADD")
+			_phoneBook.addContact();
+		if (commandString == "SEARCH")
+			_phoneBook.searchContact();
+		if (commandString == "EXIT")
+			break;
+	}
+}
