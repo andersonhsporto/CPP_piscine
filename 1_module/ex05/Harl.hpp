97eb77b0433c2b1a3e5ae871bb073050aebcfb8e
andersonhsporto <anderson.higo2@gmail.com>
2022-08-17 22:32:16 +0000

    add: working Harl

--- 1_module/ex05/Harl.hpp
+++ 1_module/ex05/Harl.hpp
@@ -5,8 +5,19 @@
 #ifndef EX05_HARL_HPP
 #define EX05_HARL_HPP
 
-
 #include <string>
+#include <iostream>
+
+#define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-\
+ketchup burger. I really do!"
+
+#define INFO_MSG "I cannot believe adding extra bacon costs more money. You didn’t put\
+enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+
+#define WARNING_MSG "I think I deserve to have some extra bacon for free. I’ve been coming for\
+years whereas you started working here since last month."
+
+#define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
 
 class Harl
 {
--- 1_module/ex05/main.cpp
+++ 1_module/ex05/main.cpp
@@ -4,6 +4,15 @@ int main()
 {
     Harl harl;
 
-    harl.complain("Teste");
+    std::cout << "Test DEBUG comment:" << std::endl;
+    harl.complain("DEBUG");
+    std::cout << "Test INFO comment:" << std::endl;
+    harl.complain("INFO");
+    std::cout << "Test WARNING comment:" << std::endl;
+    harl.complain("WARNING");
+    std::cout << "Test ERROR comment:" << std::endl;
+    harl.complain("ERROR");
+    std::cout << "Test 'Invalid' comment:" << std::endl;
+    harl.complain("Invalid #$#$#$#$");
     return 0;
 }
