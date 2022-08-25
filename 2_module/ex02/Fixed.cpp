40e548b3557eb2f3dbfeb7709f7f4e83fa9e7d3b
andersonhsporto <anderson.higo2@gmail.com>
2022-08-25 00:56:10 -0300

    delete: define messages

--- 2_module/ex02/Fixed.cpp
+++ 2_module/ex02/Fixed.cpp
@@ -9,28 +9,23 @@ const int Fixed::fractionalBits(8);
 
 Fixed::Fixed(): rawBits(0)
 {
-    std::cout << CONSTRUCTOR << std::endl;
 }
 
 Fixed::Fixed(const int integer): rawBits(integer << fractionalBits)
 {
-    std::cout << INT_CONSTRUCTOR << std::endl;
 }
 
 Fixed::Fixed(const float float_nbr): rawBits(roundf(float_nbr * (1 << fractionalBits)))
 {
-    std::cout << FLOAT_CONSTRUCTOR << std::endl;
 }
 
 Fixed::Fixed(const Fixed& fixed)
 {
-    std::cout << COPY_CONSTRUCTOR << std::endl;
     *this = fixed;
 }
 
 int Fixed::getRawBits( void ) const
 {
-    std::cout << GETTER << std::endl;
     return rawBits;
 }
 
@@ -41,7 +36,6 @@ void Fixed::setRawBits(int const raw)
 
 Fixed& Fixed::operator=(const Fixed& fixed)
 {
-    std::cout << ASSIGNMENT << std::endl;
     if (this != &fixed)
     {
         this->rawBits = fixed.rawBits;
@@ -51,7 +45,6 @@ Fixed& Fixed::operator=(const Fixed& fixed)
 
 Fixed::~Fixed()
 {
-    std::cout << DESTRUCTOR << std::endl;
 }
 
 float Fixed::toFloat(void) const
--- 2_module/ex02/Fixed.hpp
+++ 2_module/ex02/Fixed.hpp
@@ -5,13 +5,6 @@
 #ifndef EX02_FIXED_HPP
 #define EX02_FIXED_HPP
 
-#define CONSTRUCTOR         "Default constructor called"
-#define COPY_CONSTRUCTOR    "Copy constructor called"
-#define GETTER              "getRawBits member function called"
-#define ASSIGNMENT          "Copy assignment operator called"
-#define DESTRUCTOR          "Destructor called"
-#define INT_CONSTRUCTOR     "Int constructor called"
-#define FLOAT_CONSTRUCTOR   "Float constructor called"
 
 #include <cmath>
 #include <iostream>
