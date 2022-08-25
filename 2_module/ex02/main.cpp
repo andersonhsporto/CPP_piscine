5f342502a8275097245c32e6f6da47b505ba8f6c
andersonhsporto <anderson.higo2@gmail.com>
2022-08-25 00:51:54 -0300

    add: operator > and <

--- 2_module/ex02/main.cpp
+++ 2_module/ex02/main.cpp
@@ -10,15 +10,7 @@ int main( void ) {
 
     a = Fixed( 1234.4321f );
 
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-
-    std::cout << "a is " << a.toInt() << " as integer " << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer " << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer " << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer " << std::endl;
-
+    if (b < c)
+        std::cout << "OK" << std::endl;
     return 0;
 }
