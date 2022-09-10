c6b55c542b3e6d1e9d2f25a0c9def6081b9a93cb
andersonhsporto <anderson.higo2@gmail.com>
2022-09-10 02:18:44 -0300

    refactor: update tests

--- 4_module/ex00/main.cpp
+++ 4_module/ex00/main.cpp
@@ -1,16 +1,17 @@
 
 
 #include "Animal.hpp"
+#include "Cat.hpp"
 
 int main()
 {
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
+//	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+//	std::cout << j->GetType() << " " << std::endl;
+	std::cout << i->GetType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
-	j->makeSound();
+//	j->makeSound();
 	meta->makeSound();
 
 	return 0;
