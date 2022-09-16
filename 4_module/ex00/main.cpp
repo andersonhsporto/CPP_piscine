f5311bcf29cb33c194e8dd9cdadb1c3296927255
andersonhsporto <anderson.higo2@gmail.com>
2022-09-16 00:52:56 -0300

    refactor: add dog on subject test

--- 4_module/ex00/main.cpp
+++ 4_module/ex00/main.cpp
@@ -2,17 +2,21 @@
 
 #include "Animal.hpp"
 #include "Cat.hpp"
+#include "Dog.hpp"
 
 int main()
 {
 	const Animal* meta = new Animal();
-//	const Animal* j = new Dog();
+	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-//	std::cout << j->GetType() << " " << std::endl;
+	std::cout << j->GetType() << " " << std::endl;
 	std::cout << i->GetType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-//	j->makeSound();
+	i->makeSound();
+	j->makeSound();
 	meta->makeSound();
 
+    delete(meta);
+    delete(j);
+    delete(i);
 	return 0;
 }
