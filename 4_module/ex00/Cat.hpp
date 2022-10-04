61f16164995e41e389a5cf783ecb1f609cc21207
andersonhsporto <anderson.higo2@gmail.com>
2022-10-04 00:47:10 -0300

    fix: cat class

--- 4_module/ex00/Cat.hpp
+++ 4_module/ex00/Cat.hpp
@@ -11,6 +11,7 @@
 
 class Cat : public Animal
 {
+public:
     Cat();
 
     Cat(std::string& type);
--- 4_module/ex00/main.cpp
+++ 4_module/ex00/main.cpp
@@ -2,20 +2,21 @@
 
 #include "Animal.hpp"
 #include "Dog.hpp"
+#include "Cat.hpp"
 
 int main()
 {
     const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-//	const Animal* i = new Cat();
+    const Animal* j = new Dog();
+	const Animal* i = new Cat();
 //	std::cout << "Getter: " << j->GetType() << " " << std::endl;
 //	std::cout << "Getter: " << i->GetType() << " " << std::endl;
-//	i->makeSound();
-	j->makeSound();
+	i->makeSound(); //will output the cat sound!
+    j->makeSound(); //will output the dog sound!
     meta->makeSound();
 
     delete (meta);
-    delete(j);
-//    delete(i);
+    delete (j);
+    delete(i);
     return 0;
 }
