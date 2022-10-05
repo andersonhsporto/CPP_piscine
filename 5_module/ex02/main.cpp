b95957276ea1cf8c3f25deecd8b0e13c873f62e2
andersonhsporto <anderson.higo2@gmail.com>
2022-10-05 02:58:42 -0300

    add: tests

--- 5_module/ex02/main.cpp
+++ 5_module/ex02/main.cpp
@@ -2,76 +2,14 @@
 #define GREEN "\033[1;32m"
 
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
-
-void testFormGradeTooLow();
-
-void testFormGradeTooHigh();
-
-void testSignFormOk();
-
-void testSignFormLowGrade();
+#include "ShrubberyCreationForm.hpp"
 
 int main() {
-  testFormGradeTooHigh();
-  testFormGradeTooLow();
-  testSignFormOk();
-  testSignFormLowGrade();
+  ShrubberyCreationForm *shrub = new ShrubberyCreationForm("home");
+  Bureaucrat *bureaucrat = new Bureaucrat("Bureaucrat", 1);
+
+    std::cout << GREEN << "Bureaucrat sign form" << std::endl;
+    shrub->beSigned(*bureaucrat);
+    shrub->execute(*bureaucrat);
   return 0;
-}
-
-void testFormGradeTooHigh() {
-  std::cout << GREEN << "------------BEGIN TEST------------" << RESET << std::endl;
-  try {
-    Form *form = new Form("Form", 0, 1);
-
-    std::cout << *form << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  std::cout << GREEN << "------------END TEST------------" << RESET << std::endl;
-}
-
-void testFormGradeTooLow() {
-  std::cout << GREEN << "------------BEGIN TEST------------" << RESET << std::endl;
-  try {
-    Form *form = new Form("Form", 151, 1);
-
-    std::cout << *form << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  std::cout << GREEN << "------------END TEST------------" << RESET << std::endl;
-}
-
-void testSignFormOk() {
-  std::cout << GREEN << "------------BEGIN TEST------------" << RESET << std::endl;
-  Bureaucrat *bureaucrat = new Bureaucrat("Anderson", 1);
-  Form *form = new Form("42a", 1, 1);
-
-  try {
-    form->beSigned(*bureaucrat);
-    std::cout << *form << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  delete bureaucrat;
-  delete form;
-  std::cout << GREEN << "------------END TEST------------" << RESET << std::endl;
-}
-
-void testSignFormLowGrade() {
-  std::cout << GREEN << "------------BEGIN TEST------------" << RESET << std::endl;
-  Bureaucrat *bureaucrat = new Bureaucrat("Anderson", 150);
-  Form *form = new Form("42a", 1, 1);
-
-  try {
-    form->beSigned(*bureaucrat);
-    std::cout << *form << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  delete bureaucrat;
-  delete form;
-  std::cout << GREEN << "------------END TEST------------" << RESET << std::endl;
 }
