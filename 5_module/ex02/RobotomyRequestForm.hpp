6e6db6314eb9bb66b975eccadcb3e9970fcf60d5
andersonhsporto <anderson.higo2@gmail.com>
2022-10-06 01:48:05 -0300

    add: RobotomyRequestForm class

---
+++ 5_module/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,30 @@
+//
+// Created by Anderson Porto on 10/6/22.
+//
+
+#ifndef EX02_ROBOTOMYREQUESTFORM_HPP
+#define EX02_ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+
+ public:
+  RobotomyRequestForm(const std::string &target);
+
+  RobotomyRequestForm(const RobotomyRequestForm &form);
+
+  RobotomyRequestForm &operator=(const RobotomyRequestForm &form);
+
+  ~RobotomyRequestForm();
+
+  virtual void execute(const Bureaucrat &executor) const;
+
+ private:
+  const std::string target;
+
+  RobotomyRequestForm();
+
+};
+
+#endif //EX02_ROBOTOMYREQUESTFORM_HPP
