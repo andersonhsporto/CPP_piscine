1be5bed577385b82fbce18d8dc84c84339ccb9fa
andersonhsporto <anderson.higo2@gmail.com>
2022-10-06 01:47:52 -0300

    refactor: reformat code

--- 5_module/ex02/ShrubberyCreationForm.cpp
+++ 5_module/ex02/ShrubberyCreationForm.cpp
@@ -8,11 +8,13 @@ ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target) : AForm(
     "ShrubberyCreationForm",
     145,
     137), target(target) {
+
   std::cout << "ShrubberyCreationForm constructor called" << std::endl;
 }
 
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &form)
     : AForm(form), target(form.target) {
+
   std::cout << "ShrubberyCreationForm copy constructor" << std::endl;
   *this = form;
 }
@@ -31,10 +33,6 @@ ShrubberyCreationForm::~ShrubberyCreationForm() {
   std::cout << "ShrubberyCreationForm destructor called" << std::endl;
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm() {
-  std::cout << "ShrubberyCreationForm default constructor called" << std::endl;
-}
-
 void ShrubberyCreationForm::execute(const Bureaucrat &executor) const {
   AForm::execute(executor);
   std::string filename = this->target + "_shrubbery";
@@ -50,4 +48,12 @@ void ShrubberyCreationForm::execute(const Bureaucrat &executor) const {
   file << "  _ -  | |   -_" << std::endl;
   file << "      // \\\\" << std::endl;
   file.close();
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm(
+    "ShrubberyCreationForm",
+    145,
+    137), target("default") {
+
+  std::cout << "ShrubberyCreationForm default constructor called" << std::endl;
 }
