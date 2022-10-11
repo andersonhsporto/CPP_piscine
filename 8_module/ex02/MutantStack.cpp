00689449451b6732d0460e2e82d53c39232283fa
andersonhsporto <anderson.higo2@gmail.com>
2022-10-11 01:22:31 -0300

    add: basic struct ex02

---
+++ 8_module/ex02/MutantStack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anderson Porto on 10/11/22.
+//
+
+#include "MutantStack.hpp"
---
+++ 8_module/ex02/MutantStack.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Anderson Porto on 10/11/22.
+//
+
+#ifndef EX02_MUTANTSTACK_HPP
+#define EX02_MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+
+};
+
+#endif //EX02_MUTANTSTACK_HPP
