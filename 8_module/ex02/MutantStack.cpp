fc15beb85031a7f0fe415db8b4efdc16c2041a42
andersonhsporto <anderson.higo2@gmail.com>
2022-10-11 03:23:36 -0300

    add: basic struct ex02

--- 8_module/ex02/MutantStack.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Anderson Porto on 10/11/22.
-//
-
-#include "MutantStack.hpp"
--- 8_module/ex02/MutantStack.hpp
+++ 8_module/ex02/MutantStack.hpp
@@ -6,9 +6,29 @@
 #define EX02_MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
-template <typename T>
+template<typename T>
 class MutantStack : public std::stack<T> {
+
+ public:
+  MutantStack() : std::stack<T>() {}
+
+  MutantStack(MutantStack const &other) : std::stack<T>(other) {}
+
+  virtual ~MutantStack() {}
+
+  MutantStack &operator=(MutantStack const &other) {
+    if (this != &other)
+      std::stack<T>::operator=(other);
+    return *this;
+  }
+
+  typedef typename std::deque<T>::iterator iterator;
+
+  iterator begin() { return std::stack<T>::c.begin(); }
+
+  iterator end() { return std::stack<T>::c.end(); }
 
 };
 
