569e65c5ef89d5266b2df93e9fb66b5e52de3952
Anderson Porto <anderson.higo2@gmail.com>
2023-01-09 02:15:42 -0300

    Update megaphone.cpp

--- 0_module/ex00/megaphone.cpp
+++ 0_module/ex00/megaphone.cpp
@@ -2,13 +2,15 @@
 
 int main(int argc, char *argv[]) {
   if (argc != 1) {
-    for (int i = 1; i < argc; i++) {
+    int i = 1;
+    do {
       for (int j = 0; argv[i][j] != '\0'; j++) {
         std::cout << (char)toupper(argv[i][j]);
       }
       if (i != argc - 1)
         std::cout << " ";
-    }
+      i++;
+    } while (i < argc);
     std::cout << "" << std::endl;
     return 0;
   } else {
